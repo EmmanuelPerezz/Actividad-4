f18cb01a7972422c9439013dcd72e577a75b193c
Emmanuel Perez <152453287+EmmanuelPerezz@users.noreply.github.com>
2023-11-29 19:36:28 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+include('config/config.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Servicios Psicológicos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <?php include('menu.php') ?>
+    
+    <div class="container">
+        <div class="row py-5">
+            <div class="col-12 my-auto">
+                <div class="row text-center">
+                    <div class="col-lg-12 mb-4 d-flex flex-column justify-content-center aling-items-center">
+                        <div class=" index-card card  copyable">
+                            <div class="card-body ">
+                                <h1 class="card-title text-primary mb-5">¡Prioriza la salud mental! 🌈 </h1>
+                                <p class="card-text px-5">
+                                    Descubre nuestros servicios psicológicos personalizados. Con nuestra plataforma, los
+                                    psicólogos pueden gestionar fácilmente citas a través de un calendario integrado.
+                                </p>
+                                <p>¡Construyamos juntos un camino hacia el bienestar emocional! 🧠✨</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ menu.php
@@ -0,0 +1,14 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="/servicios_psicologicos"> Inicio</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="<?= ROOT ?>/Paciente/index.php"> Ver Calendario</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="<?= ROOT ?>/Paciente/add.php"> Registar Sesión </a>
+        </li>
+
+    </ul>
+</nav>
